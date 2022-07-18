5474bcbefe80aca9b1763346f895d6f56d7bce0c
Dzubayyan Ahmad <masdzub@gmail.com>
2022-07-18 13:13:27 +0700

    comment imunify

--- blockipattack.sh
+++ blockipattack.sh
@@ -3,5 +3,5 @@ cat /web/ip.list | while read line
 do
 	ip=$(echo $line)
 	csf -d $ip
-	imunify360-agent blacklist ip add $ip --scope group
+#	imunify360-agent blacklist ip add $ip --scope group
 done
